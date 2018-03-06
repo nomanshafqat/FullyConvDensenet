ebdd345491e2e0a92620f04282541bbbd7d7a48d
Noman Shafqat <14besemshafqat@seecs.edu.pk>
2018-03-06 19:29:51 +0500

    wrap

--- Net/densenet.py
+++ Net/densenet.py
@@ -7,8 +7,8 @@ class FConvDenseNet():
                  input_shape=(None, 416, 416, 3),
                  n_classes=11,
                  n_filters_first_conv=48,
-                 n_pool=5,
-                 growth_rate=12,
+                 n_pool=4,
+                 growth_rate=16,
                  n_layers_per_block=(4, 5, 7, 10, 12, 15, 12, 10, 7, 5, 4),
                  dropout_p=0.2):
 
@@ -19,8 +19,7 @@ class FConvDenseNet():
         self.growth_rate = growth_rate
         self.n_layers_per_block = n_layers_per_block
         self.dropout_p = dropout_p
-
-        return
+        self.compression=1
 
     def conv(self, inp, n_filters, kernel_size=3):
         batchnorm = tf.layers.batch_normalization(inp)
@@ -37,7 +36,6 @@ class FConvDenseNet():
     def downSample(self, inputs, filters):
 
         conv = self.conv(inputs, n_filters=filters, kernel_size=1)
-
         maxpooled = slim.max_pool2d(conv, kernel_size=2, stride=2)
         return maxpooled
 
@@ -83,24 +81,27 @@ class FConvDenseNet():
             with tf.variable_scope("block" + str(i + 1)):
                 for j in range(self.n_layers_per_block[i]):
                     with tf.variable_scope("layer" + str(j + 1)):
-                        conv = self.conv(stack, self.growth_rate)
 
-                        stack = tf.concat([stack, conv], axis=-1)
+                        with tf.variable_scope("1x1conv"):
+                            conv=self.conv(stack,self.growth_rate*4,kernel_size=1)
 
-                        n_filters += self.growth_rate
+                        with tf.variable_scope("3x3conv"):
+                            conv = self.conv(conv, self.growth_rate,kernel_size=3)
 
-                        print(stack.get_shape())
+                    stack = tf.concat([stack, conv], axis=-1)
+
+                    n_filters += self.growth_rate
+                    print(stack.get_shape(), " n=",n_filters)
 
                 pool_heads.append(stack)
 
             if self.n_pool == i:
                 continue
             with tf.variable_scope("Downsample" + str(i + 1)):
-
-                stack = self.downSample(stack, n_filters)
+                stack = self.downSample(stack, int(n_filters*0.5))
+                print(n_filters/2)
 
         stack = pool_heads[len(pool_heads) - 1]
-        print("Pooleheads len:", pool_heads)
 
         print("Turning up:", stack.get_shape())
 
@@ -110,25 +111,28 @@ class FConvDenseNet():
             with tf.variable_scope("Upsample" + str(i + 1)):
 
                 concat = pool_heads[len(pool_heads) - i - 2]
-
                 n_filters_keep = self.growth_rate * self.n_layers_per_block[self.n_pool + i]
-
-                stack = self.upsample(stack, concat, n_filters_keep)
+                print(n_filters)
+                stack = self.upsample(stack, concat, int(n_filters_keep*self.compression))
 
             with tf.variable_scope("unpool-block" + str(i + 1)):
 
                 for j in range(self.n_layers_per_block[self.n_pool + i + 1]):
                     with tf.variable_scope("layer" + str(j + 1)):
-                        conv = self.conv(stack, self.growth_rate)
-                        stack = tf.concat([stack, conv], axis=-1)
 
-                        print(stack.get_shape())
+                        with tf.variable_scope("1x1conv" + str(j + 1)):
+                            conv=self.conv(stack,self.growth_rate*4,kernel_size=1)
+                        with tf.variable_scope("3x3conv" + str(j + 1)):
+                            conv = self.conv(conv, self.growth_rate)
+
+                    stack = tf.concat([stack, conv], axis=-1)
+                    print(stack.get_shape())
 
         with tf.variable_scope("last-conv"):
 
             batchnorm = tf.layers.batch_normalization(stack)
 
-            logits = slim.conv2d(inputs=batchnorm, num_outputs=n_filters, kernel_size=1, padding='SAME',
+            logits = slim.conv2d(inputs=batchnorm, num_outputs=self.n_classes+1, kernel_size=1, padding='SAME',
                                weights_initializer=self.initializer, biases_initializer=self.initializer_b,
                                activation_fn=None)
             print("logits=", logits.get_shape())
@@ -141,7 +145,7 @@ class FConvDenseNet():
         #print(p)
         for i,a in enumerate(p):
             if(i%8)==3:
-                print(i)
+                #print(i)
                 tf.summary.histogram(a.name, tf.Graph.get_tensor_by_name(tf.get_default_graph(), a.name))
 
         # tf.summary.histogram("bias", kernel)
--- train.py
+++ train.py
@@ -1,21 +1,25 @@
 import os
+
 import numpy as np
-from  Net.densenet import FConvDenseNet
 import tensorflow as tf
+
+from DataHandler.Data_handler import Data_handler
+from  Net.densenet import FConvDenseNet
 from Net.loss import loss_func
 
 
 def train(load, ckpt_dir, gpu, lr, ckpt_steps, batchsize, imgdir, groundtruth):
+    dataHandler= Data_handler(data_location=imgdir, ground_truth=groundtruth)
+    img, labels = dataHandler.get_batch(batch_size=batchsize)
 
     input_img_size=416
 
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu)
     session_config = tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options)
 
-    batch_plc = tf.placeholder(tf.float32, [None, input_img_size, input_img_size, 3])
+    batch_plc = tf.placeholder(tf.float32, [None, input_img_size, input_img_size, 6])
     gt_plc = tf.placeholder(tf.int32, [None, input_img_size, input_img_size])
-
-    densenet = FConvDenseNet(n_classes=2,n_pool=5,growth_rate=12,n_layers_per_block=(4,4,4,4,4,4,4,4,4,4,4,4,4))
+    densenet = FConvDenseNet(n_classes=2,n_pool=5,growth_rate=16)
 
     logits, softmax = densenet.inference(batch_plc)
 
@@ -40,9 +44,10 @@ def train(load, ckpt_dir, gpu, lr, ckpt_steps, batchsize, imgdir, groundtruth):
             start = load
         while True:
 
-            img = np.ones((batchsize, input_img_size, input_img_size, 3), dtype=np.float32)
-            labels = np.ones((batchsize, input_img_size, input_img_size), dtype=np.int32)
-
+            #img = np.ones((batchsize, input_img_size, input_img_size, 3), dtype=np.float32)
+            #labels = np.ones((batchsize, input_img_size, input_img_size), dtype=np.int32)
+            img,labels=dataHandler.get_batch(batch_size=batchsize)
+            #print(labels)
             _, loss = sess.run([train_step, loss_op], feed_dict={batch_plc: img,
                                                                  gt_plc: labels,
                                                                  })
@@ -65,5 +70,20 @@ def train(load, ckpt_dir, gpu, lr, ckpt_steps, batchsize, imgdir, groundtruth):
             start += 1
 
 
+import sys
+
 if __name__ == '__main__':
-    train(-1,"ckpt",0.5,0.01,1000,1,"s","s")
+    load = int(sys.argv[1])
+    ckpt_dir = sys.argv[2]
+    gpu = float(sys.argv[3])
+    lr = float(sys.argv[4])
+    ckpt_steps = int(sys.argv[5])
+    batchsize = int(sys.argv[6])
+    imgdir = sys.argv[7]
+    groundtruth = sys.argv[8]
+
+    assert (os.path.exists(ckpt_dir))
+    assert (os.path.exists(imgdir))
+    assert (os.path.exists(groundtruth))
+
+    train(load,ckpt_dir,gpu,lr,ckpt_steps,batchsize,imgdir,groundtruth)
